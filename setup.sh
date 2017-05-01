23e7b10460398779f86fdff5d4145819e00ff27b
Gautham <gauthamm2015@gmail.com>
2017-05-01 02:40:18 -0700

    install instruction

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get install nodejs;
+sudo apt-get install npm;
+sudo npm install -g bower;
+bower install;
+sudo npm install -g gulp;
+sudo npm install gulp --save-dev;
+sudo npm install jshint gulp-jshint jshint-stylish gulp-imagemin gulp-concat gulp-uglify gulp-minify-css gulp-usemin gulp-cache gulp-changed gulp-rev gulp-rename gulp-notify  browser-sync del --save-dev;
+sudo npm install gulp-ng-annotate --save-dev;
+bower install angular-route -S;
+bower install angular-ui-router -S;
+sudo npm install json-server -g;
+bower install angular-resource -S;
